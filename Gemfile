874fd79fc5be8cb92101269584a2814702d1fc5f
ohsunmi <ohsun14@gmail.com>
2014-01-02 16:56:35 -0500

    sift logo, font sizes, thank you message

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.0.0'
 gem 'rails', '4.0.2'
 
 gem 'pg'
+gem 'validates_email_format_of'
 
 gem 'roadie'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
 		user = User.create(user_params)
 		if user.save
 			email = user.email
-			RequestInvite.send_email(email).deliver
+			RequestInvites.send_email(email).deliver
 
 			# url ="https://us3.api.mailchimp.com/1.3/?method=listSubscribe&apikey=a138e6caaa8ecd7cecaa7af04d176617-us3&id=a9dffbb9f8&email_address=" +email
 			# response = HTTParty.post(url)
