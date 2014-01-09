fcc3e4537ef6fa2f64584dc0437a93910b6a996e
ohsunmi <ohsun14@gmail.com>
2014-01-09 18:13:09 -0500

    adding js/ new regex

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
 	}
 
 	function validateEmail(email) {
-    var regex = /\S+@\S+\.\S+/;
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
 	}
 
